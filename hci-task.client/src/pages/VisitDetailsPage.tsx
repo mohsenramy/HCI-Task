19d2009398de55b20cdfd699deae4ed01ffa6dd0
Mohsen! <abdelmohsen@outlook.ie>
2024-01-07 19:37:46 +0000

    Fix wrong message displayed to user upon retrieving VisitDetails

--- hci-task.client/src/pages/VisitDetailsPage.tsx
+++ hci-task.client/src/pages/VisitDetailsPage.tsx
@@ -1,33 +1,33 @@
-import { useEffect, useState } from 'react'
-import PageContainer from '../components/PageContainer'
-import VisitDetails from '../components/VisitDetails'
-import { visitAPI } from '../api/visitAPI'
-import { Link, useParams } from 'react-router-dom'
-import { Visit } from '../@types/visitTypes'
+import { useEffect, useState } from "react";
+import PageContainer from "../components/PageContainer";
+import VisitDetails from "../components/VisitDetails";
+import { visitAPI } from "../api/visitAPI";
+import { Link, useParams } from "react-router-dom";
+import { Visit } from "../@types/visitTypes";
 
 const VisitDetailsPage = () => {
   const { visitId } = useParams();
 
-  const [visitDetails, setVisitDetails] = useState<Visit | null>(null)
-  const [loading, setLoading] = useState(false)
+  const [visitDetails, setVisitDetails] = useState<Visit | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const retrieveVisitDetails = async () => {
     try {
-      setLoading(true)
-      const visitData = await visitAPI.getVisitDetails(visitId!)
-      setVisitDetails(visitData)
+      setLoading(true);
+      const visitData = await visitAPI.getVisitDetails(visitId!);
+      setVisitDetails(visitData);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     if (visitId) {
       retrieveVisitDetails();
     }
-  }, [visitId])
+  }, [visitId]);
 
   return (
     <PageContainer>
@@ -36,16 +36,20 @@ const VisitDetailsPage = () => {
           <h2 className="font-bold text-lg">Visit Details</h2>
           <Link to="/">Home</Link>
         </div>
-        {loading && <div className='flex justify-center items-center text-gray-500 h-16'>
-          Retrieving Visit Data ...
-        </div>}
-        {visitDetails ? <VisitDetails visit={visitDetails!} /> :
-          <div className='flex justify-center items-center text-red-400 h-16'>
+        {loading ? (
+          <div className="flex justify-center items-center text-gray-500 h-16">
+            Retrieving Visit Data ...
+          </div>
+        ) : visitDetails ? (
+          <VisitDetails visit={visitDetails!} />
+        ) : (
+          <div className="flex justify-center items-center text-red-400 h-16">
             No Data Available!
-          </div>}
+          </div>
+        )}
       </div>
     </PageContainer>
-  )
-}
+  );
+};
 
-export default VisitDetailsPage
+export default VisitDetailsPage;
